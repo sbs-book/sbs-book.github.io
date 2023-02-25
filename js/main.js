b15b76702dfd5eb89e3b5d3e6e1df23220c0cc86
Cyrille Rossant <rossant@users.noreply.github.com>
2023-02-25 18:40:28 +0100

    Number animation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
+function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        obj.innerHTML = Math.floor(progress * (end - start) + start);
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        }
+    };
+    window.requestAnimationFrame(step);
+}
+
 window.onload = (ev) => {
     let links = document.querySelectorAll("header #menu li a");
     let input = document.querySelector("header #menu input");
+
     for (let link of links) {
         link.onclick = (e) => {
             input.checked = !input.checked;
         };
     }
+
+    // Number animations.
+    let animated = false;
+    var observer = new IntersectionObserver(function (entries) {
+        if (entries[0].isIntersecting === true && !animated) {
+            animateValue(document.querySelector("#numbers .col1 .number"), 0, 32, 1500);
+            animateValue(document.querySelector("#numbers .col2 .number"), 0, 8, 500);
+            animateValue(document.querySelector("#numbers .col3 .number"), 0, 16, 1000);
+            animated = true;
+        }
+    }, { threshold: [0] });
+    observer.observe(document.querySelector("#numbers"));
 };
